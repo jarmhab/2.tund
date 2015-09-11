f6a35e5e4ba0067d3fc26aca455e58e9e783e69b
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2015-09-11 11:39:31 +0300

    logic added

--- hello world.php
+++ hello world.php
@@ -8,3 +8,21 @@
 	echo $first_name. " ".$last_name;
 
 ?>
+
+<br>
+<?php
+//kommentaar: kui on v'iksem, kui 18, siis alaealine, kui on suurem siis t'isealine
+	$age = 5;
+	
+	if($age < 18) {
+		//t6ene
+		echo "alaealine";
+		
+	} else {
+		
+	
+		//v22r
+		echo "täisealine";
+	}
+
+?>
